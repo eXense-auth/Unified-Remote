b33af4aefc51fb6030f7fa7e38487a4b1a3e2ff2
eXense-auth <53921092+eXense-auth@users.noreply.github.com>
2019-11-30 16:39:21 +0300

    Add files via upload

--- FS Client/remote_win.lua
+++ FS Client/remote_win.lua
@@ -1,5 +1,6 @@
 local win = libs.win;
-local keyboard = libs.keyboard;
+local kb = libs.keyboard;
+local server = libs.server;
 
 function toast ()
 	if (settings.toast == "") then
@@ -15,7 +16,8 @@ end
 
 --@help Launch FS Client
 actions.launch = function()
-	os.start("shell:AppsFolder\24831TirrSoft.FS_7dqv9t6ww56qc!App");
+	server.update({ type = "message", text = "Opening..." });
+	os.start("explorer.exe", "shell:AppsFolder\\24831TirrSoft.FS_7dqv9t6ww56qc!App");
 end
 
 --@help Focus app
@@ -25,141 +27,198 @@ actions.switch = function()
 	win.switchtowait(hwnd);
 end
 
+--@help Kill a task or process
+actions.kill = function ()
+	win.kill("FSClient.exe");
+end
+
 --@help Lower volume
 actions.volume_down = function()
 	toast();
-	keyboard.stroke("down");
+	kb.stroke("down");
 end
 
 --@help Raise volume
 actions.volume_up = function()
 	toast();
-	keyboard.stroke("up");
+	kb.stroke("up");
 end
 
 --@help Lower volume half
 actions.volume_down_half = function()
-	keyboard.stroke("ctrl", "down");
+	kb.stroke("ctrl", "down");
 end
 
 --@help Raise volume half
 actions.volume_up_half = function()
-	keyboard.stroke("ctrl", "up");
+	kb.stroke("ctrl", "up");
 end
 
 --@help Lower volume double
 actions.volume_down_double = function()
-	keyboard.stroke("shift","down");
+	kb.stroke("shift","down");
 end
 
 --@help Raise volume double
 actions.volume_up_double = function()
-	keyboard.stroke("shift","up");
+	kb.stroke("shift","up");
 end
 
 --@help Rewind 
 actions.rewind = function()
-	keyboard.stroke("left");
+	kb.stroke("left");
 end
 
 --@help Fast forward
 actions.fast_forward = function()
-	keyboard.stroke("right");
+	kb.stroke("right");
 end
 
 --@help Rewind half
 actions.rewind_half = function()
-	keyboard.stroke("ctrl", "left");
+	kb.stroke("ctrl", "left");
 end
 
 --@help Fast forward half
 actions.fast_forward_half = function()
-	keyboard.stroke("ctrl", "right");
+	kb.stroke("ctrl", "right");
 end
 
 --@help Rewind double
 actions.rewind_double = function()
-	keyboard.stroke("shift", "left");
+	kb.stroke("shift", "left");
 end
 
 --@help Fast forward double
 actions.fast_forward_double = function()
-	keyboard.stroke("shift", "right");
+	kb.stroke("shift", "right");
 end
 
 --@help Play previous item
 actions.previous = function()
-	keyboard.stroke("shift", "P");
+	kb.stroke("shift", "P");
 end
 
 --@help Play next item
 actions.next = function()
-	keyboard.stroke("shift", "N");
+	kb.stroke("shift", "N");
 end
 
 --@help Toggle fullscreen
 actions.fullscreen = function()
-	keyboard.stroke("F");
+	kb.stroke("F");
 end
 
 --@help Exit fullscreen
 actions.exit_fullscreen = function()
-	keyboard.stroke("esc");
+	kb.stroke("esc");
 end
 
 --@help Toggle play/pause
 actions.play_pause = function()
-	keyboard.stroke("space");
-	
+	kb.stroke("space");
 end
 
 --@help Slower
 actions.slower = function()
-	keyboard.stroke("shift", "oem_comma");
+	kb.stroke("shift", "oem_comma");
 end
 
 --@help Faster
 actions.faster = function()
-	keyboard.stroke("shift", "oem_period");
+	kb.stroke("shift", "oem_period");
 end
 
 --@help Back
 actions.back = function()
-	keyboard.stroke("back");
-	
+	kb.stroke("back");
 end
 --@help Enter
 actions.enter = function()
-	keyboard.stroke("enter");
-	
+	kb.stroke("enter");
+end
+--@help Tab
+actions.tab = function()
+	kb.stroke("tab");
 end
 --@help Toggle Mute
 actions.mute = function()
-	keyboard.stroke("M");
-	
+	kb.stroke("M");
 end
 
 --@help Toggle Playlist
 actions.list = function()
-	keyboard.stroke("alt" , "E");
-	
+	kb.stroke("alt" , "E");
 end
 
 --@help Send
 actions.send = function()
-	keyboard.stroke("alt" , "C");
-	
+	kb.stroke("alt" , "C");
 end
 
 --@help Video Configuration
 actions.config = function()
-	keyboard.stroke("alt" , "S");
-	
+	kb.stroke("alt" , "S");
 end
-
 
 --@help Toggle Always On Top
 actions.on_top = function()
-	keyboard.stroke("alt" , "W");
-	
+	kb.stroke("alt" , "W");
 end
+
+--@help Step
+actions.s0 = function()
+	kb.stroke("0");
+end
+
+actions.s1 = function()
+	kb.stroke("1");
+end
+
+actions.s2 = function()
+	kb.stroke("2");
+end
+
+actions.s3 = function()
+	kb.stroke("3");
+end
+
+actions.s4 = function()
+	kb.stroke("4");
+end
+
+actions.s5 = function()
+	kb.stroke("5");
+end
+
+actions.s6 = function()
+	kb.stroke("6");
+end
+
+actions.s7 = function()
+	kb.stroke("7");
+end
+
+actions.s8 = function()
+	kb.stroke("8");
+end
+
+actions.s9 = function()
+	kb.stroke("9");
+end
+
+actions.start = function()
+	kb.stroke("home");
+end
+
+actions["end"] = function()
+	kb.stroke("end");
+end
+
+--@help NumLock
+actions.num = function()
+	kb.stroke("num");
+end
+
+
+
