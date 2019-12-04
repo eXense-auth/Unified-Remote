546838feadd2809111c310cd09bd14978f34cdf0
eXense-auth <53921092+eXense-auth@users.noreply.github.com>
2019-12-04 21:13:20 +0300

    Add files via upload

--- AIMP/remote.lua
+++ AIMP/remote.lua
@@ -1,4 +1,5 @@
 ﻿local win = libs.win;
+local fs = libs.fs;
 local timer = libs.timer
 local utf8 = libs.utf8;
 local server = libs.server;
@@ -6,20 +7,20 @@ local path = settings.path;
 
 -- Commands
 local WM_COMMAND 			= 0x111;
-local WM_USER 				= 0x400;
-local WM_AIMP_COMMAND  = WM_USER + 0x75;
-local WM_AIMP_NOTIFY   = WM_USER + 0x76;
-local WM_AIMP_PROPERTY = WM_USER + 0x77;
-local GET = 0;
-local SET = 1;
-local tid = -1;
-local playing = false;
-local title = "";
-local shuffleon = 0;
-local repeaton = 0;
-local muteon = 0;
-local vol = 0;
-local pos = 0;
+local WM_USER				= 0x400;
+local WM_AIMP_COMMAND		= WM_USER + 0x75;
+local WM_AIMP_NOTIFY		= WM_USER + 0x76;
+local WM_AIMP_PROPERTY		= WM_USER + 0x77;
+local GET					= 0;
+local SET					= 1;
+local tid					= -1;
+local playing				= false;
+local shuffleon				= 0;
+local repeaton				= 0;
+local muteon				= 0;
+local vol					= 0;
+local pos					= 0;
+local WAtitle				= "";
 
 --@help Launcher AIMP application
 actions.launch = function()	
@@ -37,6 +38,7 @@ actions.launch = function()
 	end
 end
 
+--@help Focus AIMP application
 events.focus = function ()
 	hwnd = win.find("AIMP2_RemoteInfo", nil);
 	if (hwnd==0) then
@@ -44,58 +46,50 @@ events.focus = function ()
 	else
 		tid = timer.interval(actions.update, 500);
 	end
-	checkvolume ();
-	checkprogress ();
-	checkshuffle ();
-	checkrepeat ();
-	checkmute ();
 end
 
 events.blur = function ()
 	timer.cancel(tid);
 end
 
---@help Update status information
-local WAhwnd;
 actions.update = function ()
-	if (hwnd==0) then
-		return;
-	end
+
+	checkvolume ();
+	checkprogress ();
+	checkshuffle ();
+	checkrepeat ();
+	checkmute ();
 	
+	local WAhwnd;
 	local WAhwnd = win.find("Winamp v1.x", nil);
-	local _title = win.title(WAhwnd);
-	local _playing = true;
+	local WAtitle = win.title(WAhwnd);
+	WAtitle = utf8.replace(WAtitle, " - Winamp", "");
+	server.update({ id = "info", text = WAtitle});
 	
-	if (utf8.endswith(_title, " - Winamp [paused]")) then
-		_playing = false;
-		_title = utf8.replace(_title, " - Winamp", "\n");
-	elseif (utf8.endswith(_title, " - Winamp [stopped]")) then
-		_playing = false;
-		_title = utf8.replace(_title, " - Winamp", "\n");
-	elseif (utf8.endswith(_title, " - Winamp")) then
-		_playing = true;
-		_title = utf8.replace(_title, " - Winamp", "");
+	state = win.send(hwnd, WM_AIMP_PROPERTY, 0x40 + GET, 0);
+	if (state == 0) then
+		server.update({ id = "state", icon="play", text = "Stopped" });
+		playing = false;
+	elseif (state == 1) then
+		server.update({ id = "state", icon="play", text = "Paused" });
+		playing = false;
+	elseif (state == 2) then
+		server.update({ id = "state", icon="pause", text = "Playing" });
+		playing = true;
 	else
-		_playing = false;
-		_title = "[Not Playing]";
-	end
-	
-	server.update({ id = "info", text = _title });
-	
-	if (_playing ~= playing) then
-		playing = _playing;
 	end
 	
 	if (playing) then
-		server.update({ id = "p", icon = "pause" });
+		server.update({ id = "playpause", icon = "pause" });
 	else
-		server.update({ id = "p", icon = "play" });
+		server.update({ id = "playpause", icon = "play" });
 	end
 end
 
 --@help Volume control
 actions.volume = function (volume)
 	win.send(hwnd, WM_AIMP_PROPERTY, 0x50 + SET, volume);
+	checkvolume ();
 end
 
 function checkvolume ()
@@ -104,35 +98,31 @@ function checkvolume ()
 end
 
 actions.volume_down = function()
-	actions.volume(win.send(hwnd, WM_AIMP_PROPERTY, 0x50 + GET, 0) - 10);
-	checkvolume ()
+	actions.volume(win.send(hwnd, WM_AIMP_PROPERTY, 0x50 + GET, 0) - 5);
 end
 
 actions.volume_up = function()
-	actions.volume(win.send(hwnd, WM_AIMP_PROPERTY, 0x50 + GET, 0) + 10);
-	checkvolume ()
+	actions.volume(win.send(hwnd, WM_AIMP_PROPERTY, 0x50 + GET, 0) + 5);
 end
 
 --@help Progress control
-actions.progress = function (progress)
-	win.send(hwnd, WM_AIMP_PROPERTY, 0x20 + SET, progress);
+actions.progress = function (position)
+	win.send(hwnd, WM_AIMP_PROPERTY, 0x20 + SET, position);
+	checkprogress ();
 end
 
 function checkprogress ()
 	local lenght = win.send(hwnd, WM_AIMP_PROPERTY, 0x30 + GET, 0);
 	local pos = win.send(hwnd, WM_AIMP_PROPERTY, 0x20 + GET, 0);
 	server.update({ id = "progressslider", progressmax = lenght, progress = pos });
-	local cvr = win.send(hwnd, WM_AIMP_PROPERTY, 29, 0);
 end
 
 actions.rewind = function ()
 	actions.progress(win.send(hwnd, WM_AIMP_PROPERTY, 0x20 + GET, 0) - 5000);
-	checkprogress ();
 end
 
 actions.forward = function ()
 	actions.progress(win.send(hwnd, WM_AIMP_PROPERTY, 0x20 + GET, 0) + 5000);
-	checkprogress ();
 end
 
 --@help Shuffle control
@@ -142,7 +132,7 @@ actions.shuffle = function()
 	else
 		win.send(hwnd, WM_AIMP_PROPERTY, 0x80 + SET, 0);
 	end
-	checkshuffle();
+	checkshuffle ();
 end
 
 function checkshuffle ()
@@ -161,7 +151,7 @@ actions["repeat"] = function()
 	else
 		win.send(hwnd, WM_AIMP_PROPERTY, 0x70 + SET, 0);
 	end
-	checkrepeat();
+	checkrepeat ();
 end
 
 function checkrepeat ()
@@ -180,7 +170,7 @@ actions.mute = function()
 	else
 		win.send(hwnd, WM_AIMP_PROPERTY, 0x60 + SET, 0);
 	end
-	checkmute();
+	checkmute ();
 end
 
 function checkmute ()
@@ -224,7 +214,6 @@ actions.visualisation = function()
 		win.send(hwnd, WM_AIMP_PROPERTY, 0xA0 + SET, 1);
 	else
 		win.send(hwnd, WM_AIMP_COMMAND, 31, 0);
---		win.send(hwnd, WM_AIMP_PROPERTY, 0xA0 + SET, 0);
 	end
 end
 
