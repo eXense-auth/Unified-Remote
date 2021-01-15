6cd368bdcd06cfec44a2815ddc6823aa51cde57c
eXense-auth <53921092+eXense-auth@users.noreply.github.com>
2021-01-15 03:50:17 +0300

    Universal remote for custom hotkeys
    Universal remote for custom hotkeys:
    Ctrl+F1-F12
    Alt+F1-F12
    Shift+F1-F12

---
+++ Universal Remote/remote.lua
@@ -0,0 +1,169 @@
+
+keyboard = libs.keyboard;
+
+--@help 
+
+--@help Lower system volume
+actions.volume_down = function()
+	keyboard.press("volumedown");
+end
+
+--@help Mute system volume
+actions.volume_mute = function()
+	keyboard.press("volumemute");
+end
+
+--@help Raise system volume
+actions.volume_up = function()
+	keyboard.press("volumeup");
+end
+
+--
+
+actions.cf1 = function()
+	kb.stroke("ctrl", "f1");
+end
+
+actions.cf2 = function()
+	kb.stroke("ctrl", "f2");
+end
+
+actions.cf3 = function()
+	kb.stroke("ctrl", "f3");
+end
+
+actions.cf4 = function()
+	kb.stroke("ctrl", "f4");
+end
+
+actions.cf5 = function()
+	kb.stroke("ctrl", "f5");
+end
+
+actions.cf6 = function()
+	kb.stroke("ctrl", "f6");
+end
+
+actions.cf7 = function()
+	kb.stroke("ctrl", "f7");
+end
+
+actions.cf8 = function()
+	kb.stroke("ctrl", "f8");
+end
+
+actions.cf9 = function()
+	kb.stroke("ctrl", "f9");
+end
+
+actions.cf10 = function()
+	kb.stroke("ctrl", "f10");
+end
+
+actions.cf11 = function()
+	kb.stroke("ctrl", "f11");
+end
+
+actions.cf12 = function()
+	kb.stroke("ctrl", "f12");
+end
+
+--
+
+actions.af1 = function()
+	kb.stroke("alt", "f1");
+end
+
+actions.af2 = function()
+	kb.stroke("alt", "f2");
+end
+
+actions.af3 = function()
+	kb.stroke("alt", "f3");
+end
+
+actions.af4 = function()
+	kb.stroke("alt", "f4");
+end
+
+actions.af5 = function()
+	kb.stroke("alt", "f5");
+end
+
+actions.af6 = function()
+	kb.stroke("alt", "f6");
+end
+
+actions.af7 = function()
+	kb.stroke("alt", "f7");
+end
+
+actions.af8 = function()
+	kb.stroke("alt", "f8");
+end
+
+actions.af9 = function()
+	kb.stroke("alt", "f9");
+end
+
+actions.af10 = function()
+	kb.stroke("alt", "f10");
+end
+
+actions.af11 = function()
+	kb.stroke("alt", "f11");
+end
+
+actions.af12 = function()
+	kb.stroke("alt", "f12");
+end
+
+--
+
+actions.sf1 = function()
+	kb.stroke("shift", "f1");
+end
+
+actions.sf2 = function()
+	kb.stroke("shift", "f2");
+end
+
+actions.sf3 = function()
+	kb.stroke("shift", "f3");
+end
+
+actions.sf4 = function()
+	kb.stroke("shift", "f4");
+end
+
+actions.sf5 = function()
+	kb.stroke("shift", "f5");
+end
+
+actions.sf6 = function()
+	kb.stroke("shift", "f6");
+end
+
+actions.sf7 = function()
+	kb.stroke("shift", "f7");
+end
+
+actions.sf8 = function()
+	kb.stroke("shift", "f8");
+end
+
+actions.sf9 = function()
+	kb.stroke("shift", "f9");
+end
+
+actions.sf10 = function()
+	kb.stroke("shift", "f10");
+end
+
+actions.sf11 = function()
+	kb.stroke("shift", "f11");
+end
+
+actions.sf12 = function()
+	kb.stroke("shift", "f12");
+end
